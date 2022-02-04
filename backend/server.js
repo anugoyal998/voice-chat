e0557c69e826138ae98b2b50efad2d35732925d1
anugoyal998 <anugoyal998@gmail.com>
2022-02-04 16:19:45 +0530

    useWebRTC

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,47 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 5000
-const Router = require('./routes')
-const cors = require('cors')
-const dbConnect = require('./database')
-const cookieParser = require('cookie-parser')
-dbConnect()
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const Router = require("./routes");
+const cors = require("cors");
+const dbConnect = require("./database");
+const cookieParser = require("cookie-parser");
+const server = require("http").createServer(app);
+const ACTIONS = require("./actions");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: process.env.FRONTEDN_URL,
+    method: ["GET", "POST"],
+  },
+});
+dbConnect();
 
-app.use(express.json({limit: '8mb'}))
-app.use(cookieParser())
-app.use(cors({origin: [process.env.FRONTEDN_URL],credentials: true}))
-app.use('/',Router)
-app.use('/storage',express.static('storage')) 
+app.use(express.json({ limit: "8mb" }));
+app.use(cookieParser());
+app.use(cors({ origin: [process.env.FRONTEDN_URL], credentials: true }));
+app.use("/", Router);
+app.use("/storage", express.static("storage"));
 
-app.get('/', (req, res) => {
-    res.send("<h1>Hello World!</h1>")
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
 
-app.listen(PORT,()=> {
-    console.log('listening on port 5000')
-})
+// sockets
+const socketUserMapping = {};
+
+io.on("connection", (socket) => {
+  console.log("new connection", socket.id);
+  socket.on(ACTIONS.JOIN, ({ roomId, user }) => {
+    socketUserMapping[socket.id] = user;
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+    clients.forEach((clientId) => {
+      io.to(clientId).emit(ACTIONS.ADD_PEER, {});
+    });
+    socket.emit(ACTIONS.ADD_PEER, {});
+    socket.join(roomId);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log("listening on port 5000");
+});
--- frontend/src/pages/Room/Room.jsx
+++ frontend/src/pages/Room/Room.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useWebRTC } from "../../hooks/useWebRTC";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Room = () => {
-  return <div>
-      
-  </div>
+  const { id: roomId } = useParams();
+  const user = useSelector((state) => state.auth.user);
+  const { clients, provideRef } = useWebRTC(roomId, user);
+  return (
+    <div>
+      <h1>All Connected clients</h1>
+      {clients.map((client) => {
+        return (
+          <div key={client.id}>
+            <audio
+              ref={(instance) => provideRef(instance, client.id)}
+              controls
+              autoPlay
+            ></audio>
+            <h4>{client.name}</h4>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Room;
